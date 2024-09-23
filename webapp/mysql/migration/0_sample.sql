de249f457bccab1467b72657e2d0b274e3f79c43
クレ・タカハセ <kuretakahase@64-150.eduroam.hiroshima-u.ac.jp>
2024-09-23 14:09:20 +0900

    fix index

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,4 +1,4 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
-CREATE INDEX idx_last_name ON users (username);
-CREATE INDEX idx_last_name ON users (id);
-CREATE INDEX idx_last_name ON dispatchers (user_id);
+CREATE INDEX idx_users_username ON users (username);
+CREATE INDEX idx_users_id ON users (id);
+CREATE INDEX idx_dispatchers_userid ON dispatchers (user_id);
